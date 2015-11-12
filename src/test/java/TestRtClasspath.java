79dfd72cbda598a0a39cdbe21afb2c68f4809d34
Patrick Flynn <paflynn@google.com>
2015-11-12 17:00:05 -0500

    test case that shows the intellij gradle plugin making ToXMLStream disappear off the classpath

---
+++ src/test/java/TestRtClasspath.java
@@ -0,0 +1,13 @@
+import com.sun.org.apache.xml.internal.serializer.ToXMLStream;
+
+import org.junit.Test;
+
+
+public class TestRtClasspath {
+
+  @Test
+  public void testToXMLStreamOnClassPath() {
+    ToXMLStream stream = new ToXMLStream();
+  }
+
+}
